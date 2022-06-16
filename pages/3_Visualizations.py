a592b45bfd8fd846cb8cb3b8b8e6f4a94078f8cc
Prathima Kadari <kadariprathima4@gmail.com>
2022-06-16 14:31:01 +0530

    Added Visualizations file

---
+++ pages/3_Visualizations.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+import folium
+import geopandas as gpd
+import pandas as pd
+import plotly_express as px
+import streamlit as st
+from streamlit_folium import folium_static
+
+PAGE_TITLE = "Visualizations"
