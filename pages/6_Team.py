1a4ca778c20f2ad7dd19680e8c77bac0cec6a1e7
Prathima Kadari <kadariprathima4@gmail.com>
2022-06-16 14:28:51 +0530

    Added team file

---
+++ pages/6_Team.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(page_title="Team")
+
+st.sidebar.header("Team")
+
+st.subheader('Collaborators')
+
+st.markdown("""
+[A](https://www.linkedin.com/in/)  
+[b](https://www.linkedin.com/in/)  
+[C](https://www.linkedin.com/in/)  
+[D](https://www.linkedin.com/in/)    
+""")
+
+st.subheader('Project Manager')
+
+st.markdown('[Shrey Arora](https://www.linkedin.com/in//)')
