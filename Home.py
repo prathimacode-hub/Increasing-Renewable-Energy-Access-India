39ec403e3d858ba8f85da3a5de481bcf2ec06fb8
Prathima Kadari <kadariprathima4@gmail.com>
2022-06-16 16:01:19 +0530

    Updated

--- Home.py
+++ Home.py
@@ -17,20 +17,9 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-#https://media.istockphoto.com/photos/engineer-standing-in-solar-power-station-looking-sunrise-picture-id1318137351?s=612x612
-
-st.header("Renewable Energy Access in India")
-
-#IMG_URL = 'https://media.istockphoto.com/photos/engineer-standing-in-solar-power' \
-#          '-station-looking-sunrise-picture-id1318137351?s=612x612/'
-
-#https://news.stanford.edu/2015/11/23/plan-energy-storage-112315/
-    
-#IMG_URL = 'https://etimg.etb2bimg.com/thumb/msid-79151507,width-700,resizemode-4/.jpg'
+st.header("Renewable Energy Access in India")    
 
 IMG_URL = 'https://bsmedia.business-standard.com/_media/bs/img/article/2021-12/28/full/1640711472-4731.jpg?im=Resize,width=480'
-
-#IMG_URL = 'https://news.stanford.edu/news/2015/november/images/15891-plan_news.jpg/'
 
 response = requests.get(IMG_URL)
 img = Image.open(BytesIO(response.content))
@@ -49,5 +38,5 @@ with col1:
     hope to encourage citizen science by open sourcing the dataset and code.
     """)
 with col2:
-    st.image(img.resize((500, 600)))
+    st.image(img.resize((400, 600)))
     
