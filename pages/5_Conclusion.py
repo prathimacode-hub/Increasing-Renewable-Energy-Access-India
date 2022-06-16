922c5d9956f2835d5fa8824a1ce6d6ec7025bc73
Prathima Kadari <kadariprathima4@gmail.com>
2022-06-16 14:26:08 +0530

    Added Conclusion file

---
+++ pages/5_Conclusion.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Conclusion")
+
+st.sidebar.header("Conclusion")
+
+st.subheader("Tech stack")
+st.markdown("""
+* Data Collection
+* Data Preprocessing & EDA
+* Data Visualizations
+* Modelling Prediction
+* Deployment
+""")
+
+st.subheader("Project summary")
+st.markdown("""
+*  
+*  
+*  
+*  
+""")
+
+st.subheader("Conclusion")
+st.markdown("""
+*  
+*  
+*  
+""")
