f7033cba94b54ce7fd8dd60c0fcbff3f088ec304
Prathima Kadari <kadariprathima4@gmail.com>
2022-06-16 14:23:29 +0530

    Added About section

---
+++ pages/1_About.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="About")
+
+st.sidebar.header("About")
+
+st.subheader("About the project")
+
+st.markdown("""
+##### Goals of our project  
+*  
+*  
+*  
+*  
+""")
+
+
+st.markdown("""
+##### Developments we made  
+*  
+*  
+""")
