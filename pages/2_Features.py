553eabba46a464689ab2200a799bd11585676b15
Prathima Kadari <kadariprathima4@gmail.com>
2022-06-16 16:08:20 +0530

    Updated Features file

--- pages/2_Features.py
+++ pages/2_Features.py
@@ -3,3 +3,13 @@ import streamlit as st
 st.set_page_config(page_title="Features")
 
 st.sidebar.header("Features")
+
+st.subheader("Features")
+
+st.markdown("""
+##### Feature Endorsements
+
+* 
+* 
+* 
+""")
