32ca23a1a5a4de2cc1eb718a0dd1e33d8051a038
Prathima Kadari <kadariprathima4@gmail.com>
2022-06-16 16:12:23 +0530

    Updated Conclusion file

--- pages/5_Conclusion.py
+++ pages/5_Conclusion.py
@@ -5,15 +5,6 @@ st.set_page_config(page_title="Conclusion")
 
 st.sidebar.header("Conclusion")
 
-st.subheader("Tech stack")
-st.markdown("""
-* Data Collection
-* Data Preprocessing & EDA
-* Data Visualizations
-* Modelling Prediction
-* Deployment
-""")
-
 st.subheader("Project summary")
 st.markdown("""
 *  
