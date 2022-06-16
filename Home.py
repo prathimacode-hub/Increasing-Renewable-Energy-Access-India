3bb238afccdc445e68b1df797f9f74694a29ebb0
Prathima Kadari <kadariprathima4@gmail.com>
2022-06-16 15:58:32 +0530

    Updated Home file

--- Home.py
+++ Home.py
@@ -39,7 +39,14 @@ col1, col2 = st.columns(2)
 with col1:
     st.subheader('Increasing Our Renewable Energy Access Throughout India using AI')
     st.markdown("""
-    efnfnkdnfkdnfkdnfk.
+    India has vast land reserves and is geographically favourable for solar power projects, but there is still scope to improve the electricity 
+    production that relies on coal.
+    
+    This initiative is taken to use satellite data in conjunction with other relevant datasets to identify sites that are most suitable for 
+    solar panel installations as a greener energy source through machine learning and coverage analysis.
+    
+    The aim is to help connect and encourage organisations to use AI tools to understand and plan in transition for green energy. We also 
+    hope to encourage citizen science by open sourcing the dataset and code.
     """)
 with col2:
     st.image(img.resize((500, 600)))
