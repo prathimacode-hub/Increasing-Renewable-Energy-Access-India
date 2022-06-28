6a8ca9cb74537f249f0ce0044ef581e90037c52c
Prathima Kadari <kadariprathima4@gmail.com>
2022-06-28 15:32:47 +0530

    Update 3_Visualizations.py

--- pages/3_Visualizations.py
+++ pages/3_Visualizations.py
@@ -112,7 +112,7 @@ folium.TileLayer('cartodbdark_matter').add_to(m2)
 folium.LayerControl().add_to(m2)
 
 df = pd.read_csv('Solar_GHI.csv') #global horizontal irradiance
-df = pd.read_csv('Solar_DNI.csv') #direct normal irradiance
+#df = pd.read_csv('Solar_DNI.csv') #direct normal irradiance
 
 df['LONG'] = pd.to_numeric(df.LONG, errors='coerce')
 df['LAT'] = pd.to_numeric(df.LAT, errors='coerce')# drop rows with missing lat and lon
