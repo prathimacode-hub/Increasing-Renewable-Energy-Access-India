6a0106dab8725030b4e05a748ec4366da8778389
Prathima Kadari <kadariprathima4@gmail.com>
2022-07-10 20:01:03 +0530

    Update 3_Visualizations.py

--- pages/3_Visualizations.py
+++ pages/3_Visualizations.py
@@ -32,6 +32,10 @@ from branca.element import Figure
 from geopy.geocoders import Nominatim
 import aspose.words as aw
 
+import streamlit.components.v1 as components
+import os
+from pylocator.positionstackfuns import latlonpositionstack
+
 #from turfpy.measurement import bbox
 #from functools import reduce
 
@@ -126,6 +130,57 @@ map_1 = KeplerGl(height=800)
 map_1.add_data(data=df, name="monthly-temperatures-of-india")
 keplergl_static(map_1)
 
+
+
+
+if "latitude" not in st.session_state.keys():
+    st.session_state["latitude"] = 20.0504188
+if "longitude" not in st.session_state.keys():
+    st.session_state["longitude"] = 64.4139099
+if "cityname" not in st.session_state.keys():
+    st.session_state["cityname"] = "New Delhi"
+
+st.title("Weather Map using Streamlit")
+
+st.markdown("""
+    Welcome to this weather map. To use this application, simply select the input 
+    type, which can be either the **city name** or the **latitude and longitude** 
+    of the place that you want, then click the **Search** button. The map 
+    will then change and show the weather of the place.
+""")
+
+st.markdown("---")
+
+selectope = st.selectbox("Select input type", ["City name", "Latitude and Longitude"])
+
+with st.form(key="location_input"):
+    if selectope == "City name":
+        locat = st.text_input("Type the city name below", value="New Delhi")
+    elif selectope == "Latitude and Longitude":
+        latinput = st.number_input("Latitude",value=20.0504188)
+        loninput = st.number_input("Longitude", value=64.4139099)
+    subbutton = st.form_submit_button("Search")
+
+if subbutton:
+    if selectope == "City name":
+
+        # Get the latitude and longitude
+        latdata, londata = latlonpositionstack(
+            os.environ.get("apipositionstack"),
+            locat)
+        st.session_state["latitude"] = latdata
+        st.session_state["longitude"] = londata
+
+    elif selectope == "Latitude and Longitude":
+        # Get the latitude and longitude
+        st.session_state["latitude"] = latinput
+        st.session_state["longitude"] = loninput
+
+latc = st.session_state["latitude"]
+lonc = st.session_state["longitude"]
+
+components.iframe(src=f"https://embed.windy.com/embed2.html?lat={latc}&lon={lonc}&detailLat={latc}&detailLon={lonc}&width=650&height=450&zoom=5&level=surface&overlay=wind&product=ecmwf&menu=&message=&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1",
+    height=500)
 
 
 
